61c158851d6fce99630beb49fa17af3605be7a38
Mats Bergman <eubmabe@yahoo.com>
2017-03-17 13:17:18 +0100

    echo TSHARK command

--- TSHARK.bat
+++ TSHARK.bat
@@ -3,5 +3,6 @@ echo Starting Tshark on interface %4 for %3 seconds
 REM "C:\Program Files\Wireshark\tshark.exe" -a duration:%2 -o rtp.heuristic_rtp:FALSE -q -z rtp,streams -i %3 > %1
 REM "C:\Program Files\Wireshark\tshark.exe" -a duration:%2 -d udp.port==50040:20,rtp -o rtp.heuristic_rtp:FALSE -q -z rtp,streams -i %3 > %1
 %1 -a duration:%3 -d udp.port==50040:20,rtp -o rtp.heuristic_rtp:TRUE -q -z rtp,streams -i %4 > %2
+echo %1 -a duration:%3 -d udp.port==50040:20,rtp -o rtp.heuristic_rtp:TRUE -q -z rtp,streams -i %4 > %2
 
 exit
